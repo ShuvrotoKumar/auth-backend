5f859b32869723dddfe73d4fd46a8ddf323ae417
ShuvrotoKumar <sdks6409@gmail.com>
2026-03-08 05:33:35 +0600

    port created and connected

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ connectDB();
 
 
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(process.env.PORT, () => {
+    console.log('Server is running on port ' + process.env.PORT);
 });
