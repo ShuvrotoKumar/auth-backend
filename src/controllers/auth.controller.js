96adf6ae00ffe6662912b427ea4c71924ee13504
ShuvrotoKumar <sdks6409@gmail.com>
2026-03-09 03:03:15 +0600

    imported json web token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,9 +1,22 @@
 const userModel = require('../models/user.model');
-
+const jwt = require('jsonwebtoken');
 
 async function register(req, res) {
     const { username, email, password } = req.body;
     // TODO: Implement registration logic
+
+
+    const user = new userModel({
+        username,
+        email,
+        password
+    });
+
+    // await user.save();
+
+    // res.status(201).json({
+    //     message: 'User registered successfully'
+    // });
 }
 
 // async function login(req, res) {
