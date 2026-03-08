72c4be02df8c869452b9ae0622c28cd59ad05807
ShuvrotoKumar <sdks6409@gmail.com>
2026-03-09 01:31:12 +0600

    imported authROute in app file

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const authRoute = require('./routes/auth.route');
 
 
 
@@ -11,7 +11,7 @@ app.use(express.json());
 
 
 
-
+app.use('/api/auth', authRoute);
 
 
 
