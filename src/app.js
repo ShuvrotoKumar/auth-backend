bcc350d68983da43b4bd3b367a229f3ab010eb33
ShuvrotoKumar <sdks6409@gmail.com>
2026-03-18 00:48:00 +0600

    added cookies in server

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const authRoute = require('./routes/auth.route');
-
+const cookieParser = require('cookie-parser');
 
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 
 
