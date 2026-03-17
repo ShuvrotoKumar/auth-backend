4462a8847a927d8e1f05b65f8978819ccdbeebff
ShuvrotoKumar <sdks6409@gmail.com>
2026-03-18 02:20:31 +0600

    test get api created with token console

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -4,6 +4,10 @@ const authController = require('../controllers/auth.controller');
 const router = express.Router();
 
 router.post('/register', authController.register);
+router.get('/test', (req, res) => {
+    // console.log(req.cookies);
+    res.json({ message: 'Test route', cookies: req.cookies });
+});
 
 // router.post('/login', (req, res) => {
 //     const { email, password } = req.body;
